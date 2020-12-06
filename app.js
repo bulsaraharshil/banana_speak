988c626d62797412e114b8820ef922d489ec5274
Harshil Bulsara <bulsaraharshil@gmail.com>
2020-12-06 19:48:09 +0530

    Added banana translation with styling

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverUrl + "?" + "text=" + input
+}
+
+function errorHandler(e) {
+    console.log("Error " + e)
+}
+
+function clickHandler() {
+    var inputTxt = txtInput.value
+    fetch(getTranslationURL(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText
+            }
+        )
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
